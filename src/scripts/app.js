8d5b717649f1d513c6aca41b8219124a6df1d0b8
P0p0vKirill <kirill.popov.89@yandex.ru>
2025-05-30 14:47:53 +0300

    first commit

---
+++ src/scripts/app.js
@@ -0,0 +1,44 @@
+const select = document.querySelector("select");
+const allLangs = ["en", "ru"];
+select.addEventListener("change", changeURLLanguage);
+function changeURLLanguage() {
+  let lang = select.value;
+  location.href = window.location.pathname + "#" + lang;
+  location.reload();
+}
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.slice(1);
+  if (!allLangs.includes(hash)) {
+    location.href = window.location.pathname + "#ru";
+    location.reload();
+  }
+  select.value = hash;
+  for (key in langArr) {
+    let el = document.querySelector(".lng-" + key);
+    if (el) el.innerHTML = langArr[key][hash];
+  }
+}
+changeLanguage();
+(function titleСoloring() {
+  let wordsArr = ["Frontend-разработкой", "Frontend development"];
+  let title = document.querySelector(".lng-header-title");
+  for (let phrase of wordsArr) {
+    if (title.innerHTML.includes(phrase))
+      title.innerHTML = title.innerHTML.replace(
+        phrase,
+        `<span style="color:#C778DD">${phrase}</span>`
+      );
+  }
+})();
+
+
+// button-contact-me
+
+const contactMe = document.querySelector('.contact-me')
+const formSend = document.querySelector('.form-send-email')
+contactMe.addEventListener('click', contactMeNone);
+function contactMeNone() {
+  contactMe.classList.add('inactive')
+  formSend.classList.remove('inactive')
+}
---
+++ src/scripts/lang.js
@@ -0,0 +1,22 @@
+const langArr = {
+  main: {
+    ru: "главная",
+    en: "home",
+  },
+  works: {
+    ru: "мои работы",
+    en: "works",
+  },
+  "about-me": {
+    ru: "обо мне",
+    en: "about-me",
+  },
+  contacts: {
+    ru: "контакты",
+    en: "contacts",
+  },
+  "header-title": {
+    ru: "Привет, я Кирилл! С 2023 года занимаюсь Frontend-разработкой.",
+    en: "Hi, I'm Kirill! I have been engaged in Frontend development since 2023.",
+  },
+};
