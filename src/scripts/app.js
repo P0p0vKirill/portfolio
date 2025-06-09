517f2b8fb45648cbce731cfb2036d6371b1b1038
P0p0vKirill <kirill.popov.89@yandex.ru>
2025-06-10 00:56:50 +0300

    third commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -44,3 +44,15 @@ function contactMeNone() {
   contactMe.classList.add('inactive')
   formSend.classList.remove('inactive')
 }
+
+// nav-btn
+const nav = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const navBtnImg = document.querySelector("#nav-btn-img");
+navBtn.onclick = () => {
+  if (nav.classList.toggle('open')){
+    navBtnImg.src = './images/nav-button-close.svg';
+  } else {
+     navBtnImg.src = './images/nav-button-open.svg';
+  }
+}
